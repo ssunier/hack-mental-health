0d1192df4f9ddd5991d11c54b4b958fec76b4696
Sheridan <ssunier@wellesley.edu>
2014-11-08 14:55:43 -0500

    pushing current working version to master

--- hack_health/hack_health/urls.py
+++ hack_health/hack_health/urls.py
@@ -1,5 +1,7 @@
-from django.conf.urls import include, url
+from django.conf.urls import patterns, include, url
 from django.contrib import admin
+
+from health import views
 
 urlpatterns = [
     # Examples:
@@ -7,4 +9,5 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    #url(r'^$', views.index, name='index')
 ]
