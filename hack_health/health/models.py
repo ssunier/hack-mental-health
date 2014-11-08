41ce3e8b08d0760dc4755d820dd971dd3800ae77
Mark Chen <markchen90@gmail.com>
2014-11-08 12:07:55 -0500

    some more models

--- hack_health/health/models.py
+++ hack_health/health/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+Class Person(models.Model):
+  person_id = models.AutoField(primary_key = true)
+  phone_number = models.IntegerField()
+
+Class Person2Person(models.Model):
+  from_id = models.IntegerField()
+  to_id = models.IntegerField()
+
+Class Person2Day(models.Model):
+  person_id = models.IntegerField()
+  date = models.IntegerField()
+  date_id = models.IntegerField()
+
