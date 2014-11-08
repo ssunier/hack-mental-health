0c66b1225df5cb0eb93a1f42e065900a47e0b8b7
Joy Jiao <Joy@Joys-MacBook-Pro-2.local>
2014-11-08 17:46:35 -0500

    python parser for jawbone data added

--- hack_health/hack_health/settings.py
+++ hack_health/hack_health/settings.py
@@ -36,7 +36,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'health'
+    'health',
+    'requests',
+    'requests_oauth2'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -83,3 +85,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+	    # Use Django's standard `django.contrib.auth` permissions,
+	        # or allow read-only access for unauthenticated users.
+		'DEFAULT_PERMISSION_CLASSES': [
+		            'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+			        ]
+		}
--- hack_health/hack_health/urls.py
+++ hack_health/hack_health/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
 
 urlpatterns = [
     # Examples:
