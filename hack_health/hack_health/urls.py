8a1cfd00c2fb561c34dbb3a2df69b8e397d1409f
Sheridan <ssunier@wellesley.edu>
2014-11-08 15:29:30 -0500

    made a basic index view

--- hack_health/hack_health/urls.py
+++ hack_health/hack_health/urls.py
@@ -1,13 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-from health import views
-
 urlpatterns = [
     # Examples:
     # url(r'^$', 'hack_health.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^health', include('health.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    #url(r'^$', views.index, name='index')
 ]
