478f1a2828ce092b7787c5b6f752b0d585705772
Sheridan <ssunier@wellesley.edu>
2014-11-08 23:15:09 -0500

    put the index into a template

--- hack_health/health/views.py
+++ hack_health/health/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponseRedirect, HttpResponse, Http404
-from django.template import RequestContext, Context
+from django.template import RequestContext, Context, loader
 
 from health.models import Mood
 
 # Create your views here.
 def index(request):
-  return HttpResponse('hello you are at the index')
+  template = loader.get_template('index.html')
+  context = Context({''})
+  return HttpResponse(template.render(context))
