9305f6c4e4ab812eda45831ca13341937a907e70
Sheridan <ssunier@wellesley.edu>
2014-11-08 22:49:53 -0500

    built url file

--- hack_health/hack_health/urls.py
+++ hack_health/hack_health/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import *
 from django.contrib import admin
 
 from health import views
@@ -8,6 +8,7 @@ urlpatterns = [
     # url(r'^$', 'hack_health.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^health/', include('health.urls')),
     url(r'^admin/', include(admin.site.urls)),
     #url(r'^$', views.index, name='index')
 ]
--- hack_health/health/views.py
+++ hack_health/health/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse, Http404
+from django.template import RequestContext, Context
+
+from health.models import Mood
 
 # Create your views here.
+def index(request):
+  return HttpResponse('hello you are at the index')
